6faa8133c64d41d62f202cf378d92fe3b691dd8f
Zakki00 <zakkimubarroq06@gmail.com>
2024-11-19 22:41:24 +0700

    19/11/2024

---
+++ app/src/main/java/androidstudio/example/borrowinggoods/Admin.kt
@@ -0,0 +1,64 @@
+package androidstudio.example.borrowinggoods
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Admin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_admin)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val sharedPref = getSharedPreferences("loginPrefs", Context.MODE_PRIVATE)
+        val nama = sharedPref.getString("nama_admin", "Nama Admin tidak ditemukan")
+        val username = sharedPref.getString("username", "Username tidak ditemukan")
+
+
+        val textView1 = findViewById<TextView>(R.id.textView1)
+        val textView2 = findViewById<TextView>(R.id.textView2)
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+        val button4 = findViewById<Button>(R.id.button4)
+
+        textView1.text = nama
+        textView2.text = username
+
+        button1.setOnClickListener {
+            val intent = Intent(this, admin_barang::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            startActivity(intent)
+            finish()
+        }
+        button3.setOnClickListener {
+            val intent = Intent(this, admin_transaksi::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            startActivity(intent)
+            finish()
+        }
+        button4.setOnClickListener {
+            val intent = Intent(this, Profile::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            startActivity(intent)
+            finish()
+
+        }
+
+
+    }
+
+}
